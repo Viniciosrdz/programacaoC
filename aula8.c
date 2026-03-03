506f10705f88bc5e76d447369446738e747ec605
marcio cardoso <137656788+Viniciosrdz@users.noreply.github.com>
2026-03-03 18:35:32 -0300

    alteracoes

--- aula8.c
+++ aula8.c
@@ -5,11 +5,11 @@ int main(){
     int idade2= 30;
 
     if(idade1 > idade2){
-        printf("A idade 1 é maior que a idade 2\n");
+        printf("A pessoa 1 é mais velha que a pessoa 2\n");
     } else if(idade1 < idade2){
-        printf("A idade 2 é maior que a idade 1\n");
+        printf("A pessoa 2 é mais velha que a pessoa 1\n");
     } else {
-        printf("As idades são iguais\n");
+        printf("As pessoas têm a mesma idade\n");
     }
 
 return 0;
