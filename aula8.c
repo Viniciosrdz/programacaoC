775f1232b5e242b67436e9662d2b1dbef59552f2
marcio cardoso <137656788+Viniciosrdz@users.noreply.github.com>
2026-03-03 18:34:12 -0300

    iniciando if

---
+++ aula8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    
+    int idade1= 30;
+    int idade2= 30;
+
+    if(idade1 > idade2){
+        printf("A idade 1 é maior que a idade 2\n");
+    } else if(idade1 < idade2){
+        printf("A idade 2 é maior que a idade 1\n");
+    } else {
+        printf("As idades são iguais\n");
+    }
+
+return 0;
+}
+ 
