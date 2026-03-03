05bf029f84bda624db0e70cbd340d175e15ae0e3
marcio cardoso <137656788+Viniciosrdz@users.noreply.github.com>
2026-03-03 18:40:01 -0300

    alteracoes

--- aula8.c
+++ aula8.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main(){
     
-    int idade1= 30;
-    int idade2= 30;
+ int idade1; 
+ int idade2;
+  printf("Digite a idade da pessoa 1: ");
+    scanf("%d", &idade1);
+    
+ printf("Digite a idade da pessoa 2: ");
+  scanf("%d", &idade2);
 
     if(idade1 > idade2){
         printf("A pessoa 1 é mais velha que a pessoa 2\n");
