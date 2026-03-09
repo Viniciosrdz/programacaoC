eb63bb8885c656dde9bdb362842e9cb187f4dfd6
marcio cardoso <137656788+Viniciosrdz@users.noreply.github.com>
2026-03-09 18:41:16 -0300

    estoque

--- aula9.c
+++ aula9.c
@@ -4,15 +4,19 @@ int main(){
     int numero = 5, resultado;
     resultado = numero % 2;
 
+    int estoque = 5;
+
     printf("A variavel resultado tem o valor: %d\n", resultado);
-    
+
     if(resultado == 0){
         printf("O número %d é par\n", numero);
     } else {
         printf("O número %d é ímpar\n", numero);
     }
 
-
+    if(estoque <= 5){
+ printf("O estoque está baixo\n");
+ }
 
 
 }
